59a28c908f9ca16bc4b11004f9bfa1af7df1018f
yangjia <yangjia1416@163.com>
2025-01-06 17:21:46 +0800

    perf: 基本布局分割与路由创建

---
+++ src/layout/index.vue
@@ -0,0 +1,34 @@
+<script setup></script>
+
+<template>
+  <div id="layout">
+    <div class="top">top</div>
+    <div class="content">
+       <div class="layout-nav">nav</div>
+       <div class="layout-view">view</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+#layout {
+  height: 100vh;
+  .top {
+    height: 60px;
+    background-color: #fff;
+  }
+  .content {
+    height: calc(100vh - 60px);
+    display: flex;
+    background-color: red;
+    .layout-nav {
+      width: 200px;
+      background-color: pink;
+    }
+    .layout-view {
+      flex: 1;
+      background-color: skyblue;
+    }
+  }
+}
+</style>
